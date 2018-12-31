436bd01a39a869c14cf71aba742dc4cf85c1e693
Damodar Lohani <dlohani48@gmail.com>
2018-12-31 12:42:39 +0545

    playing audio files

--- lib/pages/book_details.dart
+++ lib/pages/book_details.dart
@@ -1,17 +1,26 @@
 import 'package:audiobooks/resources/books_api_provider.dart';
 import 'package:audiobooks/resources/models/book.dart';
 import 'package:audiobooks/widgets/title.dart';
+import 'package:audioplayer/audioplayer.dart';
 import 'package:flutter/material.dart';
 import 'package:webfeed/domain/rss_item.dart';
 
-class DetailPage extends StatelessWidget {
+class DetailPage extends StatefulWidget {
   final Book book;
-  DetailPage(this.book){
-    _getRssFeeds();
+  DetailPage(this.book);
+
+  @override
+  DetailPageState createState() {
+    return new DetailPageState();
   }
+}
+
+class DetailPageState extends State<DetailPage> {
+  bool playing = false;
+  AudioPlayer player = AudioPlayer();
 
   Future<List<RssItem>>_getRssFeeds() {
-    return BooksApiProvider().fetchFeeds(book.urlRSS);
+    return BooksApiProvider().fetchFeeds(widget.book.urlRSS);
   }
 
   @override
@@ -22,10 +31,12 @@ class DetailPage extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
-          BookTitle(book.title),
-          Text(book.description),
-          Text(book.totalTime),
-          Text(book.urlZipFile),
+          BookTitle(widget.book.title),
+          Text(widget.book.description),
+          Text(widget.book.totalTime),
+          Text(widget.book.urlZipFile),
+          SizedBox(height: 20,),
+          IconButton(icon: Icon(playing?Icons.pause:Icons.play_arrow), onPressed: _togglePlayer,),
           SizedBox(height: 20,),
           Container(
             child: FutureBuilder(
@@ -33,7 +44,10 @@ class DetailPage extends StatelessWidget {
               builder: (BuildContext context, AsyncSnapshot<List<RssItem>> snapshot){
                 if(snapshot.hasData){
                   return Column(
-                    children: snapshot.data.map((item)=>Text(item.enclosure.url)).toList(),
+                    children: snapshot.data.map((item)=>ListTile(
+                      title: Text(item.title),
+                      onTap: () => _playAudio(item.enclosure.url),
+                    )).toList(),
                   );
                 }else{
                   return CircularProgressIndicator();
@@ -45,5 +59,27 @@ class DetailPage extends StatelessWidget {
         ],
       )
     );
+  }
+
+  void _playAudio(String url) {
+    player.stop();
+    player.play(url);
+    setState(() {
+      playing = true;
+    });
+  }
+
+  void _togglePlayer(){
+    if(playing) {
+      player.pause();
+      setState(() {
+        playing = false;
+      });
+    }else{
+      player.play("");
+      setState(() {
+        playing = true;
+      });
+    }
   }
 }
